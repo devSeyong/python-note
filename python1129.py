76043ab034da071cca930fcf02002e1c11cff35e
csy <choseyong0213@gmail.com>
2024-11-30 21:39:38 +0900

    python note

--- python1129.py
+++ python1129.py
@@ -2,6 +2,5 @@
 # 출력값 : naver;kakao;sk;samsung
 
 # 정답
-print("naver", "kakao", "samsung", sep=";")
+print("naver", "kakao", "samsung",sep=";")
 #print 함수의 sep 인자로 ";"를 입력하면 출력되는 값들 사이에 한 칸의 공백대신 세미콜론이 출력됩니다.
-
